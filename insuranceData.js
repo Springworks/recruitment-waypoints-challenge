fc5017de8f96ea6b6761457541609764627a6a88
Sima <sima.boreiri@gmail.com>
2022-11-15 10:30:19 +0100

    amendments in packagejson

--- insuranceData.js
+++ insuranceData.js
@@ -8,15 +8,62 @@ const dataCalculation = (waypointsArr)=> {
     totalDuration : 0
   };
    if (waypointsArr.length < 2){
-return returnedObj
+       return returnedObj
 };
 
 };
 
+for (let i = 1; i < waypointsArr.length; ++i)
 
-//for (let i = 1; i < waypointsArr.length; ++i)
 
 
-console.log(dataCalculation(["sima"]))
+//console.log(dataCalculation(["sima"]))
 
+dataCalculation([
+  {
+    timestamp: "2016-06-21T12:00:00.000Z",
+    position: {
+      latitude: 59.334,
+      longitude: 18.0667,
+    },
+    speed: 6.3889,
+    speed_limit: 8.33,
+  },
+  {
+    timestamp: "2016-06-21T12:00:05.000Z",
+    position: {
+      latitude: 59.3337,
+      longitude: 18.0662,
+    },
+    speed: 9.4,
+    speed_limit: 8.33,
+  },
+  {
+    timestamp: "2016-06-21T12:00:10.000Z",
+    position: {
+      latitude: 59.3331,
+      longitude: 18.0664,
+    },
+    speed: 11.1,
+    speed_limit: 8.33,
+  },
+  {
+    timestamp: "2016-06-21T12:00:15.000Z",
+    position: {
+      latitude: 59.3327,
+      longitude: 18.0665,
+    },
+    speed: 8.32,
+    speed_limit: 8.33,
+  },
+  {
+    timestamp: "2016-06-21T12:00:20.000Z",
+    position: {
+      latitude: 59.3323,
+      longitude: 18.0666,
+    },
+    speed: 8.33,
+    speed_limit: 8.33,
+  },
+])
 module.exports = dataCalculation
